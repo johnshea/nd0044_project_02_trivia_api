186f88cf804c4bced0d143530e910e46b45a4a64
John Shea <sheajohnh@gmail.com>
2022-02-07 22:35:28 -0500

    Add test cases. Move user/password to env variable.

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -16,7 +16,7 @@ class TriviaTestCase(unittest.TestCase):
         self.client = self.app.test_client
         self.database_name = "trivia_test"
         self.database_path = "postgres://{}:{}@{}/{}".format(
-  'postgres', 'postgres', 'localhost:5432', self.database_name)
+  os.environ['FLASK_DB_USER'], os.environ['FLASK_DB_USER_PASSWORD'], 'localhost:5432', self.database_name)
         setup_db(self.app, self.database_path)
 
         # binds the app to the current context
@@ -45,6 +45,20 @@ class TriviaTestCase(unittest.TestCase):
         self.assertTrue(data['questions'])
         self.assertEqual(data['categories']['2'], 'Art')
 
+    def test_get_questions_with_negative_page_value(self):
+        res = self.client().get('/questions?page=-1')
+        data = res.get_json()
+
+        self.assertEqual(res.status_code, 400)
+        self.assertFalse(data['success'])
+
+    def test_get_questions_with_invalid_high_page_value(self):
+        res = self.client().get('/questions?page=1000')
+        data = res.get_json()
+
+        self.assertEqual(res.status_code, 404)
+        self.assertFalse(data['success'])
+
     def test_get_questions_per_specific_category(self):
         res = self.client().get('/categories/2/questions')
         data = res.get_json()
@@ -53,6 +67,13 @@ class TriviaTestCase(unittest.TestCase):
         self.assertEqual(data['total_questions'], 4)
         self.assertEqual(data['current_category'], 2)
         self.assertTrue(data['questions'])
+
+    def test_get_questions_per_specific_category_but_category_does_not_exist(self):
+        res = self.client().get('/categories/99/questions')
+        data = res.get_json()
+
+        self.assertEqual(res.status_code, 400)
+        self.assertFalse(data['success'])
 
     def test_get_all_categories(self):
         res = self.client().get('/categories')
@@ -83,6 +104,20 @@ class TriviaTestCase(unittest.TestCase):
         self.assertEqual(total_questions_before+1, total_questions_after)
         self.assertEqual(data['success'], True)
 
+    def test_create_new_question_with_field_missing(self):
+
+        new_question = {
+            "question": "The Big Apple is in which state?",
+            "answer": "New York",
+            "difficulty": 1,
+        }
+
+        res = self.client().post('/questions', json=new_question)
+        data = res.get_json()
+
+        self.assertEqual(res.status_code, 400)
+        self.assertEqual(data['success'], False)
+
     def test_delete_question(self):
 
         question_before = Question.query.filter(Question.id == 4).one_or_none()
@@ -97,6 +132,14 @@ class TriviaTestCase(unittest.TestCase):
         question_after = Question.query.filter(Question.id == 4).one_or_none()
         self.assertFalse(question_after)
 
+    def test_delete_question_that_does_not_exist(self):
+
+        res = self.client().delete('/questions/999')
+        data = res.get_json()
+
+        self.assertEqual(res.status_code, 400)
+        self.assertFalse(data['success'])
+
     def test_search_title(self):
         res = self.client().post('/questions', json={"searchTerm": "title"})
         data = res.get_json()
@@ -104,6 +147,20 @@ class TriviaTestCase(unittest.TestCase):
         self.assertEqual(res.status_code, 200)
         self.assertEqual(data['success'], True)
         self.assertEqual(len(data['questions']), 2)
+
+    def test_post_questions_without_body(self):
+        res = self.client().post('/questions')
+        data = res.get_json()
+
+        self.assertEqual(res.status_code, 400)
+        self.assertFalse(data['success'])
+
+    def test_post_quizzes_without_body(self):
+        res = self.client().post('/quizzes')
+        data = res.get_json()
+
+        self.assertEqual(res.status_code, 400)
+        self.assertFalse(data['success'])
 
     def test_quizzes_with_category(self):
         res = self.client().post('/quizzes', json={
@@ -141,6 +198,14 @@ class TriviaTestCase(unittest.TestCase):
         self.assertTrue(data['success'])
         self.assertIsNone(data['question'])
 
+    def test_quizzes_without_previous_questions_parameter(self):
+        res = self.client().post('/quizzes', json={
+            "quiz_category": {'type': 'History', 'id': '4'}
+        })
+        data = res.get_json()
+
+        self.assertEqual(res.status_code, 400)
+        self.assertFalse(data['success'])
 
 # Make the tests conveniently executable
 if __name__ == "__main__":
